ef05183441b8e4f4ec11414dd492194a2c2679b5
Chinu Sohlot <65461295+YoungSTAAR@users.noreply.github.com>
2021-09-07 12:28:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let screen = document.getElementById('screen');
+let screenValue = '';
+buttons = document.querySelectorAll('button');
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+
+        } else if (buttonText == "=") {
+            screen.value = eval(screenValue);
+        } else if (buttonText == "%") {
+            buttonText = '/ 100';
+            screenValue += buttonText;
+            screen.value = screenValue;
+
+        } else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
